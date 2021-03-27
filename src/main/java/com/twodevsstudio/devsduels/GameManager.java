0b590687fd702ed1cb5a3a0aab325d8b06c8fcc2
slighterr12 <patrykszot.1@gmail.com>
2021-03-27 23:55:31 +0100

    First glance at GameManager

---
+++ src/main/java/com/twodevsstudio/devsduels/GameManager.java
@@ -0,0 +1,33 @@
+package com.twodevsstudio.devsduels;
+
+import com.twodevsstudio.devsduels.base.Arena;
+import com.twodevsstudio.devsduels.base.Team;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class GameManager {
+    
+    private final Map<Arena, Team> activeGames = new HashMap<>();
+    
+    public void startDuel(Arena arena, Team team) {
+        
+        if (isStarted(arena)) {
+            return;
+        }
+        
+        activeGames.put(arena, team);
+    }
+    
+    public boolean isStarted(Arena arena) {
+        
+        return activeGames.containsKey(arena);
+    }
+    
+    public void stopDuel(Arena arena) {
+        
+        activeGames.remove(arena);
+    }
+}
