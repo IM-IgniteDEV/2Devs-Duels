bb30c02df74ddc36eb25cad98a5dcabb0a188459
ignitedev <ignitedevcontact@gmail.com>
2021-03-28 00:20:54 +0100

    Repository for Duel Players and Basic of caching edited blocks

---
+++ src/main/java/com/twodevsstudio/devsduels/repository/DuelPlayerRepository.java
@@ -0,0 +1,34 @@
+package com.twodevsstudio.devsduels.repository;
+
+import com.twodevsstudio.devsduels.base.DuelPlayer;
+import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DuelPlayerRepository {
+    
+    @Getter
+    private final Map<UUID, DuelPlayer> playerCache = new ConcurrentHashMap<>();
+    
+    @Nullable
+    public DuelPlayer findDuelPlayerByUUID(UUID uuid) {
+        
+        DuelPlayer duelPlayer = playerCache.get(uuid);
+        
+        if (duelPlayer == null) {
+            duelPlayer = new DuelPlayer(uuid);
+            addToCache(duelPlayer);
+        }
+        
+        return duelPlayer;
+    }
+    
+    public void addToCache(DuelPlayer duelPlayer) {
+        
+        playerCache.put(duelPlayer.getUuid(), duelPlayer);
+    }
+    
+}
