083060fa4b72c567443590e086c0a65a086f59e0
ignitedev <ignitedevcontact@gmail.com>
2021-03-28 00:40:03 +0100

    Active arena cache in duel player

--- src/main/java/com/twodevsstudio/devsduels/base/DuelPlayer.java
+++ src/main/java/com/twodevsstudio/devsduels/base/DuelPlayer.java
@@ -1,32 +1,13 @@
 package com.twodevsstudio.devsduels.base;
 
 import lombok.Data;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @Data
 public class DuelPlayer {
     
     private final UUID uuid;
-    private final Map<Location, Material> editedBlocks = new HashMap<>();
-    
-    public void addBlock(Location location, Material material){
-        editedBlocks.put(location, material);
-    }
-    
-    public void revertBlocks(){
-        editedBlocks.forEach((location, material) -> {
-            World world = location.getWorld();
-            
-            world.getBlockAt(location).setType(material);
-        });
-        
-        editedBlocks.clear();
-    }
+    private Arena activeArena;
     
 }
