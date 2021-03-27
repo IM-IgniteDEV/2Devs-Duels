20fcaa10f6fd5d7492ebabf06b0d248775f9bbc4
ignitedev <ignitedevcontact@gmail.com>
2021-03-27 22:43:35 +0100

    BaseUtils

---
+++ src/main/java/com/twodevsstudio/devsduels/util/BaseUtil.java
@@ -0,0 +1,16 @@
+package com.twodevsstudio.devsduels.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+@NoArgsConstructor( access = AccessLevel.PRIVATE )
+public class BaseUtil {
+    
+    public static Component colorComponent(String toColor) {
+        
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(toColor).asComponent();
+    }
+    
+}
