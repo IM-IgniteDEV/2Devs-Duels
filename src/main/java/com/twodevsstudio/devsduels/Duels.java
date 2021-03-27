51fecaf575e5b596e73dfc8a6aa307a8a86af77d
ignitedev <ignitedevcontact@gmail.com>
2021-03-27 22:40:14 +0100

    Initial commit

---
+++ src/main/java/com/twodevsstudio/devsduels/Duels.java
@@ -0,0 +1,16 @@
+package com.twodevsstudio.devsduels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Duels extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+    
+    }
+    
+    @Override
+    public void onDisable() {
+    
+    }
+}
