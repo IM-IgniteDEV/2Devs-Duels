f6217513c0a704bb8ebb4eeecfd09b5230b6db8b
ignitedev <ignitedevcontact@gmail.com>
2021-03-28 00:39:26 +0100

    Cache of original state of an arena

--- src/main/java/com/twodevsstudio/devsduels/GameManager.java
+++ src/main/java/com/twodevsstudio/devsduels/GameManager.java
@@ -31,4 +31,5 @@ public class GameManager {
         
         activeGames.remove(arena);
     }
+    
 }
--- src/main/java/com/twodevsstudio/devsduels/base/Arena.java
+++ src/main/java/com/twodevsstudio/devsduels/base/Arena.java
@@ -1,9 +1,13 @@
 package com.twodevsstudio.devsduels.base;
 
 import lombok.Data;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,5 +22,21 @@ public class Arena {
     private final Map<Integer, ItemStack> startingItems;
     private final List<ArenaLocation> spawningLocations;
     private final List<PotionEffect> potionEffects;
+    
+    private final Map<Location, Material> editedBlocks = new HashMap<>();
+    
+    public void addBlock(Location location, Material material){
+        editedBlocks.putIfAbsent(location, material);
+    }
+    
+    public void revertBlocks(){
+        editedBlocks.forEach((location, material) -> {
+            World world = location.getWorld();
+            
+            world.getBlockAt(location).setType(material);
+        });
+        
+        editedBlocks.clear();
+    }
     
 }
