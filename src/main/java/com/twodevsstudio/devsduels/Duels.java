68f75272ec17f43362c048c4c63e4805fa6f543f
ignitedev <ignitedevcontact@gmail.com>
2021-03-27 22:56:45 +0100

    Initial BaseConfiguration

--- src/main/java/com/twodevsstudio/devsduels/Duels.java
+++ src/main/java/com/twodevsstudio/devsduels/Duels.java
@@ -1,5 +1,7 @@
 package com.twodevsstudio.devsduels;
 
+import com.twodevsstudio.devsduels.configuration.BaseConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Duels extends JavaPlugin {
@@ -7,6 +9,10 @@ public final class Duels extends JavaPlugin {
     @Override
     public void onEnable() {
     
+        BaseConfiguration baseConfiguration = new BaseConfiguration();
+        FileConfiguration config = getConfig();
+        
+        baseConfiguration.initialize(config);
     }
     
     @Override
--- src/main/java/com/twodevsstudio/devsduels/configuration/BaseConfiguration.java
+++ src/main/java/com/twodevsstudio/devsduels/configuration/BaseConfiguration.java
@@ -1,4 +1,29 @@
 package com.twodevsstudio.devsduels.configuration;
 
+import com.twodevsstudio.devsduels.base.Arena;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class BaseConfiguration {
+    
+    private List<Arena> arenaList;
+
+    public void initialize(FileConfiguration fileConfiguration){
+        
+    
+        loadArenas(fileConfiguration);
+    }
+    
+    
+    private void loadArenas(FileConfiguration fileConfiguration){
+    
+    
+    
+    }
+
 }
