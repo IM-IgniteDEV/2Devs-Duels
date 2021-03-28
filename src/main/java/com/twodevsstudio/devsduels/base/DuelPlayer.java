4ec768d9882c2c4548cfcf9b4527d035368b33e7
ignitedev <ignitedevcontact@gmail.com>
2021-03-28 13:35:17 +0200

    DuelPlayer UUID notnull and active arena is nullable

--- src/main/java/com/twodevsstudio/devsduels/base/DuelPlayer.java
+++ src/main/java/com/twodevsstudio/devsduels/base/DuelPlayer.java
@@ -1,13 +1,17 @@
 package com.twodevsstudio.devsduels.base;
 
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
 @Data
 public class DuelPlayer {
     
+    @NotNull
     private final UUID uuid;
+    @Nullable
     private Arena activeArena;
     
 }
